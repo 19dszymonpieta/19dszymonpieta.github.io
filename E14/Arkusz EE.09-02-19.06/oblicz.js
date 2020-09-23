047efb59dc119239e88f8cc5b2d6b73fc707f87b
19dszymonpieta <58705413+19dszymonpieta@users.noreply.github.com>
2020-09-23 12:46:22 +0200

    Add files via upload

--- E14/Arkusz EE.09-02-19.06/oblicz.js
+++ E14/Arkusz EE.09-02-19.06/oblicz.js
@@ -2,7 +2,7 @@ function oblicz(){
 	var a = document.getElementById("a").value;
 	if(a==1){a=4}
 	else if(a==2){a=3.5}
-	else {a=0}
+	else {a==0}
 	var b = document.getElementById("b").value;
 	var wynik = a * b;
 	document.getElementById("wynik").innerHTML = "Koszt paliwa: "+wynik+" zł";
