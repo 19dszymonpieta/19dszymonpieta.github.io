5831175b07be89ccb2cb4e34d4ed045f1f775095
19dszymonpieta <58705413+19dszymonpieta@users.noreply.github.com>
2020-09-30 12:39:14 +0200

    Update oblicz.js

--- E14/Arkusz E.14-01-20.06-SG/oblicz.js
+++ E14/Arkusz E.14-01-20.06-SG/oblicz.js
@@ -1,6 +1,6 @@
 function oblicz(){
 	var a = document.getElementById("a").value;
 	let b = a % 4;
-	b > 0 ? b = (a - b) / 4 : b = a / 4;
+	b > 0 ? b = (a - b) / 4 + 1: b = a / 4;
 	document.getElementById("wynik").innerHTML = "Liczba jednolitrowych puszek potrzebnych do pomalowania wynosi " + b ;
 }
