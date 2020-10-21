83ff4f8ea21cd78ef061a2127afcb2f4a2363282
19dszymonpieta <58705413+19dszymonpieta@users.noreply.github.com>
2020-10-21 13:12:06 +0200

    Add files via upload

---
+++ E14/Arkusz EE.09-05-20.06-SG/skrypt.js
@@ -0,0 +1,9 @@
+function strzyzenie(){
+	var krotkie = document.getElementById("krotkie").value;
+	var srednie = document.getElementById("srednie").value;
+	var poldlugie = document.getElementById("poldlugie").value;
+	var dlugie = document.getElementById("dlugie").value;
+
+		document.getElementById("cena").innerHtml = "jd" + krotkie;
+
+}
