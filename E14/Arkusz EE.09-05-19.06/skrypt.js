8bc9e4b01ce85e88d18e0f4e0decda44c3d81a1b
19dszymonpieta <58705413+19dszymonpieta@users.noreply.github.com>
2020-10-28 18:40:14 +0100

    Update skrypt.js

--- E14/Arkusz EE.09-05-19.06/skrypt.js
+++ E14/Arkusz EE.09-05-19.06/skrypt.js
@@ -1,11 +1,11 @@
 function generuj(){
-			var liczba = parseInt(document.getElementById("pierwsza").value);
-			var roznica = parseInt(document.getElementById("roznica").value);
-			var liczbaWyrazow = parseInt(document.getElementById("liczbaWyrazow").value);
-			var wynik = "Ciąg arytmetyczny zawiera wyrazy: ";
-			for(var i = 0; i < liczbaWyrazow ; i++){
+			let liczba = parseInt(document.getElementById("pierwsza").value);
+			let roznica = parseInt(document.getElementById("roznica").value);
+			let liczbaWyrazow = parseInt(document.getElementById("liczbaWyrazow").value);
+			let wynik = "Ciąg arytmetyczny zawiera wyrazy: ";
+			for(let i = 0; i < liczbaWyrazow ; i++){
 				wynik += liczba+", ";
 				liczba += roznica;
 			}
 			document.getElementById("napis").innerHtml = wynik;
-		}
+}
