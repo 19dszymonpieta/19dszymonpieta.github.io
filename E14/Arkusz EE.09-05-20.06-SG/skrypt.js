53682a99e617d711daef175581217a43a09a2322
19dszymonpieta <58705413+19dszymonpieta@users.noreply.github.com>
2020-11-04 11:52:54 +0100

    Update skrypt.js

--- E14/Arkusz EE.09-05-20.06-SG/skrypt.js
+++ E14/Arkusz EE.09-05-20.06-SG/skrypt.js
@@ -1,9 +1,21 @@
 function strzyzenie(){
-	var krotkie = document.getElementById("krotkie").value;
-	var srednie = document.getElementById("srednie").value;
-	var poldlugie = document.getElementById("poldlugie").value;
-	var dlugie = document.getElementById("dlugie").value;
-
-		document.getElementById("cena").innerHtml = "jd" + krotkie;
+	var a = document.getElementById("krotkie").value;
+	var b = document.getElementById("srednie").value;
+	var c = document.getElementById("poldlugie").value;
+	var d = document.getElementById("dlugie").value;
+	var e = 0;
+	if(a.checked == true){
+		e = 25;
+	}
+	if(b.checked == true){
+		e = 30;
+	}
+	if(c.checked == true){
+		e = 40;
+	}
+	if(d.checked == true){
+		e = 50;
+	}
+	document.getElementById("cena").innerHtml = "Cena strzyżenia:" + e;
 
 }
