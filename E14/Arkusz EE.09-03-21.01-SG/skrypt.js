7039821c15edba3b1da3f13bcb8c0be1ebaba796
19dszymonpieta <58705413+19dszymonpieta@users.noreply.github.com>
2021-05-19 13:18:14 +0200

    Update skrypt.js

--- E14/Arkusz EE.09-03-21.01-SG/skrypt.js
+++ E14/Arkusz EE.09-03-21.01-SG/skrypt.js
@@ -1,6 +1,7 @@
 function oblicz() {
 	let waga = document.getElementById("waga").value;
 	let wzrost = document.getElementById("wzrost").value;
-	let bmi = waga / (wzrost**2) * 10000;
+	let bmi = waga / (wzrost*wzrost) * 10000;
+	document.getElementById("dane").innerHTML = "Twoja waga: "+waga+"; Twój wzrost: "+wzrost;
 	document.getElementById("wynik").innerHTML = "Twoje bmi: " + bmi;
 }
